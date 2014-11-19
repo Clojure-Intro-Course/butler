91c7c02b0f0f65a402ffde236c8279e8379726bc
Henry Fellows <fello056@morris.umn.edu>
2014-11-19 15:52:57 -0600

    Another try at getting a working lein plugin.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject lein-butler "0.1.0"
-  :description "Butler will, when it's done, serve up the project map and form."
+(defproject lein-butler "0.1.1"
+  :description "An experimental plugin, that may or may not be a grue."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[robert/hooke "1.3.0"]]
-  :plugins [[lein-pprint "1.1.1"]]
-  :main lein-butler.core)
+  :plugins [[lein-pprint "1.1.2"]])
--- src/leiningen/butler.clj
+++ src/leiningen/butler.clj
@@ -1,5 +1,17 @@
-(ns leiningen.butler)
+(ns leiningen.butler
+	(require [leiningen.core.eval :only [eval-in-project]]
+           [robert.hooke :only [add-hook]])))
 
-(defn butler [project & keys] (println "main ran"))
+(defn- add-wrap-eval [f project form init]
+  (let [result (f project form init)]
+    (println "Inside of wrap-eval")
+    (println result)
+    (result)
+     ))
+
+(defn lein-butler.plugin/hooks []
+  (add-hook #'eval-in-project add-wrap-eval))
+
+(defn butler "An experimental plugin, that may or may not be a grue." [project & keys] (println "main ran"))
 
 
