670ec4d3f6beb2cc9d91ae80a1150417093c77f4
MaxMagnuson <localadmin@corlar.morris.umn.edu>
2014-12-10 15:13:57 -0600

    We now know that the plugin is in fact running, and we are able to hook into leiningen.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject lein-butler "0.1.1"
-  :description "An experimental plugin, that may or may not be a grue."
+  :description "Different Description"
+  ;"An experimental plugin, that may or may not be a grue."
+  :url "https://fixme.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[robert/hooke "1.3.0"]]
-  :plugins [[lein-pprint "1.1.2"]])
+  )
+
--- src/leiningen/butler.clj
+++ src/leiningen/butler.clj
@@ -1,17 +1,6 @@
-(ns leiningen.butler
-	(require [leiningen.core.eval :only [eval-in-project]]
-           [robert.hooke :only [add-hook]])))
+(ns leiningen.butler)
 
-(defn- add-wrap-eval [f project form init]
-  (let [result (f project form init)]
-    (println "Inside of wrap-eval")
-    (println result)
-    (result)
-     ))
+(defn butler "Different Description" [project & keys] (println "Butler is running"))
 
-(defn lein-butler.plugin/hooks []
-  (add-hook #'eval-in-project add-wrap-eval))
-
-(defn butler "An experimental plugin, that may or may not be a grue." [project & keys] (println "main ran"))
 
 
