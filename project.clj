aa91aa6b941f84684011cd316eb4dec4dd3b54a8
Henry Fellows <fello056@morris.umn.edu>
2014-12-10 16:10:39 -0600

    Changing the name, one step at a time.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject lein-butler "0.1.1"
-  :description "Different Description"
-  ;"An experimental plugin, that may or may not be a grue."
-  :url "https://fixme.com"
+(defproject lein-hooke-sinker "0.2.0"
+  :description "A platform for experimenting with Leiningen and Robert Hooke"
+  :url "https://github.com/Clojure-Intro-Course/lein-hooke-sinker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
--- src/leiningen/butler.clj
+++
@@ -1,6 +0,0 @@
-(ns leiningen.butler)
-
-(defn butler "Different Description" [project & keys] (println "Butler is running"))
-
-
-
