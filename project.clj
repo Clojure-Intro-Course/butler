2be779b9052987c7c38f511ef9a82363dd7a5d76
Henry Fellows <fello056@morris.umn.edu>
2014-12-12 15:06:25 -0600

    Documentation and renaming, again.

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject lein-hooke-sinker "0.2.0"
+(defproject lein-sinker "0.2.0"
   :description "A platform for experimenting with Leiningen and Robert Hooke"
   :url "https://github.com/Clojure-Intro-Course/lein-hooke-sinker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  ;;You don't need a dependency on clojure - you use Leiningen's version of clojure.
+  ;;(1.6.0 as of lein 2.5)
+  ;;All other dependencies in Leiningen are available as well.
   :dependencies [[robert/hooke "1.3.0"]]
   )
 
--- src/leiningen/hooke_sinker.clj
+++
@@ -1,6 +0,0 @@
-(ns leiningen.hooke-sinker)
-
-(defn hooke-sinker "Essentially, the main for hooke-sinker." [project & keys] (println "You've ran hooke-sinker."))
-
-
-
